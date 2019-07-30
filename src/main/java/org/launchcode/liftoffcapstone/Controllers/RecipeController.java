84189ae1e2e34b6f1d70a6045437857d04767ca0
Dana Paquet <danapaquet@gmail.com>
2019-07-30 18:06:42 -0500

    Added thymeleaf fragments

--- src/main/java/org/launchcode/liftoffcapstone/Controllers/RecipeController.java
+++ src/main/java/org/launchcode/liftoffcapstone/Controllers/RecipeController.java
@@ -1,19 +1,24 @@
 package org.launchcode.liftoffcapstone.Controllers;
 
 
+import org.launchcode.liftoffcapstone.models.Recipe;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
+@RequestMapping(value= "recipe")
 
 public class RecipeController {
 
-    @RequestMapping(value= "")
-    @ResponseBody
-    public String index(){
-        return "its working";
+    @RequestMapping(value= "", method = RequestMethod.GET)
+    public String add(Model model){
+        model.addAttribute(new Recipe());
+        model.addAttribute("title", "Add New Recipe");
+        return "recipe/add";
+
     }
 
 
